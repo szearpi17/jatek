95aec4cbd5412027e70fe222dbfe95e6f46a817b
Vizeske <118881929+Vizeske@users.noreply.github.com>
2023-02-01 09:49:55 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import  random
+
+
+
+nev = input('Adja meg a nevet!')
+erosseg = random.randint(5,15)
+ugyesseg = random.randint (5,15)
+szerencse = random.randint (5,15)
+print('Üdvözlünk a játékban!' )
+print(f' Erősség: {erosseg} Ügyesség: {ugyesseg} Szerencse: {szerencse}')
