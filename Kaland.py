baee99f3f2a052a281759985c7590478886047d0
Vizeske <118881929+Vizeske@users.noreply.github.com>
2023-02-22 08:00:15 +0100

    Add files via upload

---
+++ Kaland.py
@@ -0,0 +1,38 @@
+import random
+
+print("Kalandjáték indítása...")
+
+# Játékos neve
+nev = input("Adja meg a játékos nevét: ")
+print('Üdvözöljük a játékban!! Sok sikert kíván Sebi és csapata')
+erosseg = random.randint(5,15)
+ugyesseg = random.randint(5,15)
+szerencse =random.randint(5,15)
+
+# Kezdeti helyzet
+print("Üdvözöllek, " + nev + "! Te egy kalandozó vagy, aki egy sötét barlangban találja magát.")
+print("Előtted most egy út áll, lapozz a 41-re")
+
+
+
+
+# Választás
+ut = input(" Győztél? (igen / nem)")
+
+if ut == "i":
+    print("Lépj a 85-re kártyára")
+elif ut == "n":
+    print("Vége a játéknak!")
+else:
+    print("Vége a játéknak!")
+
+ut = input("A Tolvajnál 3 Aranytallért és gyümölcsöt találtál.  Merre szeretnél menni? (Északra / Nyugatra)")
+
+if ut == "é":
+    print("Lépj a 108-as kártyára")
+elif ut == "ny":
+    print("Lépj a 147-es mezőre")
+
+
+
+print("Vége a játéknak.")
