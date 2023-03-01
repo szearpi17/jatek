21009b39855d347bdf273d92587545e30c3e43c8
Vizeske <118881929+Vizeske@users.noreply.github.com>
2023-03-01 08:00:55 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
 import random
+
 import json
 
-with open ("kaland.json", "r", )as f:
-    data = json.load(f)
-
-cards1 = data['cards']
-
-print("cards1")
-for cards in cards1:
-    print(f"{cards1['szöveg']}, {cards1['ugrás']}")
-
-# Játékos neve
 nev = input("Adja meg a játékos nevét: ")
 print('Üdvözöljük a játékban!! Sok sikert kíván Sebi és csapata')
 erosseg = random.randint(5,15)
@@ -21,26 +12,47 @@ szerencse =random.randint(5,15)
 print("Üdvözöllek, " + nev + "! Te egy kalandozó vagy, aki egy sötét barlangban találja magát.")
 print("Előtted most egy út áll, lapozz a 41-re")
 
+print("Üdvözöllek a Kalandjátékban! A célod, hogy átkelj a sötét erdőn.")
 
+# az elérhető irányok
+lepesek = ["balra", "jobbra"]
 
+# a játékos kezdőpozíciója
+pozicio = 0
 
-# Választás
-ut = input(" Győztél? (igen / nem)")
+# a véletlenszerűen generált akadályok
+akadalyok = [random.randint(1, 10) for _ in range(5)]
 
-if ut == "i":
-    print("Lépj a 85-re kártyára")
-elif ut == "n":
-    print("Vége a játéknak!")
-else:
-    print("Vége a játéknak!")
+# a játék fő ciklusa
+while True:
+    print("Jelenleg a(z) " + str(pozicio) + ". pozícióban vagy.")
 
-ut = input("A Tolvajnál 3 Aranytallért és gyümölcsöt találtál.  Merre szeretnél menni? (Északra / Nyugatra)")
+    # ellenőrizzük, hogy elértük-e a célvonalat
+    if pozicio == len(akadalyok):
+        print("Gratulálok, átjutottál az erdőn!")
+        break
 
-if ut == "é":
-    print("Lépj a 108-as kártyára")
-elif ut == "ny":
-    print("Lépj a 147-es mezőre")
+    # megjelenítjük az aktuális akadályt
+    print("Ez egy " + str(akadalyok[pozicio]) + " méter magas akadály.")
 
+    # kérjük a játékos választását
+    valasztas = input("Merre szeretnél menni? (balra/jobbra) ")
+
+    # ellenőrizzük, hogy a játékos érvényes lépést tett-e
+    if valasztas not in lepesek:
+        print("Hibás választás! Csak balra vagy jobbra léphetsz.")
+        continue
+
+    # végrehajtjuk a játékos lépését
+    if valasztas == "balra":
+        pozicio -= 1
+    elif valasztas == "jobbra":
+        pozicio += 1
+
+    # ellenőrizzük, hogy a játékos át tud-e menni az akadályon
+    if pozicio < len(akadalyok) and akadalyok[pozicio] > 5:
+        print("Sajnos nem tudtál átmenni az akadályon!")
+        break
 
 
 print("Vége a játéknak.")
